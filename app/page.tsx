cb632dcc2471b396e3b74a8b45baaba0f47df9be
Selwan <selwan.abdeltawab@gmail.com>
2024-11-03 17:57:40 +0100

    project setup for the food app

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import NewPost from "@/components/post-modal";
 import styles from "./page.module.css";
-import Posts from "@/components/posts";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <NewPost />
-      <Posts />
-    </main>
-  );
+  return <main className={styles.main}></main>;
 }
